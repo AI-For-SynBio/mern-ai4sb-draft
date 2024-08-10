b1d400a7f37bfd988e8cd05853b0970edc98564e
Syarwina <syarwinaaa09@gmail.com>
2024-08-10 10:43:26 +0800

    we need a loading state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast'
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-import LoginPage from './pages/LoginPage';
-import HomePage from './pages/HomePage';
-import SignUpPage from './pages/SignUpPage';
-import ExplorePage from './pages/ExplorePage';
-import LikesPage from './pages/LikesPage';
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
+import ExplorePage from "./pages/ExplorePage";
+import LikesPage from "./pages/LikesPage";
 
-import Sidebar from './components/Sidebar';
-import { useAuthContext } from './context/AuthContext';
+import Sidebar from "./components/Sidebar";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
-	const {authUser} = useAuthContext();
-	console.log("Authenticated user:", authUser)
+	const { authUser, loading } = useAuthContext();
+	console.log("Authenticated user:", authUser);
+
+	if (loading) return null;
+
 	return (
 		<div className='flex'>
 			<Sidebar />
@@ -21,8 +24,8 @@ function App() {
 					<Route path='/' element={<HomePage />} />
 					<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
 					<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
-					<Route path='/explore' element={!authUser ? <ExplorePage /> : <Navigate to={"/login"} />} />
-					<Route path='/likes' element={!authUser ? <LikesPage /> : <Navigate to={"/login"} />} />
+					<Route path='/explore' element={authUser ? <ExplorePage /> : <Navigate to={"/login"} />} />
+					<Route path='/likes' element={authUser ? <LikesPage /> : <Navigate to={"/login"} />} />
 				</Routes>
 				<Toaster />
 			</div>
@@ -30,4 +33,4 @@ function App() {
 	);
 }
 
-export default App
+export default App;
